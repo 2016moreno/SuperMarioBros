c030c748cc78d1cde0692f4542b14336d0bd932e
David Moreno <2016moreno@gmail.com>
2020-04-07 01:28:03 -0500

    Adding java files

---
+++ SuperMarioBrosOne/desktop/src/com/supermariobros/lab7/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.supermariobros.lab7.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.supermariobros.lab7.SMB;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SMB(), config);
+	}
+}
